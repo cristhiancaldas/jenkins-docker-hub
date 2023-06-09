bfe901a4fa27a8c543aec22bdd12e8fca8a49fbd
Cristhian Caldas <c.calda.m@gmail.com>
2023-06-08 21:47:45 -0500

    message service

--- src/main/java/com/app/messsage/controller/MessageController.java
+++ src/main/java/com/app/messsage/controller/MessageController.java
@@ -8,6 +8,6 @@ public class MessageController {
 
     @GetMapping
     public String messageDummy(){
-     return "Message Dummy - 2023-Bandera";
+     return "Message Dummy - 2023 - June 08";
  }
 }
